65922f01fdc4d7c0ec922e373a0b11883b4268fc
Joshua Emmons <skia@skia.net>
2019-03-20 12:31:25 -0500

    Moves delegate type into class.
    Requires Swift 3.1

--- Sources/StateMachine.swift
+++ Sources/StateMachine.swift
@@ -21,9 +21,22 @@ import Foundation
  */
 public class StateMachine<T: StateType> {
   /**
+   Delegate tasks of `StateMachine`. Consumers can assign implementations to respond to lifecycle events.
+   */
+  public struct StateTransitionDelegates {
+    /**
+     Delegate closure that gets called whenever `StateMachine` successfully transitions from one state to another.
+     
+     * Warning: Like all delegates, there's a high likelihood the delegatee owns the delegator and retain cycles are a danger. If the closure references the delegatee (even as `self`), it should be declared `weak` in a capture list. See [String Reference Cycles for Closures](https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/AutomaticReferenceCounting.html#//apple_ref/doc/uid/TP40014097-CH20-ID56).
+     */
+    public var didTransition: ((_ from: T, _ to: T) -> Void)?
+  }
+
+  
+  /**
    Consumers can assign implementations to protperties of `delegates` to respond to lifecycle events.
    */
-  public var delegates = StateTransitionDelegates<T>()
+  public var delegates = StateTransitionDelegates()
   
   
   /**
